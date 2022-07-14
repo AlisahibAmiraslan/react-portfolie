373b84323df76440bc56485392c4d8b6492705c9
Alisahib Amiraslan <=>
2022-07-14 22:50:42 +0300

    dsds

--- src/Home/Home-content/HomeContent.js
+++ src/Home/Home-content/HomeContent.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const HomeContent = () => {
-  return <div className="flex justify-end">HomeContent</div>;
+  return (
+    <div className="home-content">
+      <div className="body-content">
+        <h1>
+          Ali <span>Aslan</span>
+        </h1>
+        <p>I am a Front-end Developer</p>
+      </div>
+      <a href="#about">
+        <div className="home-mouse">
+          <div className="mouse-shape">
+            <div className="mouse-wheel"></div>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
 };
 
 export default HomeContent;
