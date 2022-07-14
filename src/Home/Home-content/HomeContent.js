345468e58cb6fdeabe21b65afd80c24b3ef646bf
Alisahib Amiraslan <=>
2022-07-14 23:24:36 +0300

    asasa

--- src/Home/Home-content/HomeContent.js
+++ src/Home/Home-content/HomeContent.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 const HomeContent = () => {
+  useEffect(() => {
+    let text = "Front-end Developer";
+    let myJob = document.querySelector(".profession");
+    let index = 0;
+    function textWrite() {
+      myJob.innerHTML = text.slice(0, index);
+      index++;
+      if (index > text.length) {
+        index = 0;
+      }
+    }
+    setInterval(textWrite, 200);
+  }, []);
+
   return (
     <div className="home-content">
       <div className="body-content">
         <h1>
           Ali <span>Aslan</span>
         </h1>
-        <p>I am a Front-end Developer</p>
+        <p>
+          I am a <span className="profession"></span>
+        </p>
       </div>
       <a href="#about">
         <div className="home-mouse">
