28f80fe10ee9f54196d3285a0b7479a7da05328a
Alisahib Amiraslan <=>
2022-07-17 02:10:26 +0300

    dddsd

--- src/Service/Service.js
+++ src/Service/Service.js
@@ -1,9 +1,6 @@
 import React from "react";
 import "./service.scss";
 import data from "./service-data";
-import { AiFillHtml5 } from "react-icons/ai";
-import { BsImages, BsWordpress, BsArrowsMove, BsGear } from "react-icons/bs";
-import { FaRocket } from "react-icons/fa";
 
 function Service() {
   console.log(data);
@@ -18,78 +15,22 @@ function Service() {
         </span>
       </div>
       <div className="my-services grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 mt-10">
-        <div className="bg-white m-5 text-center p-10 border-2">
-          <div className="icons flex justify-center">
-            <AiFillHtml5 className="w-12 h-12 text-red-600" />
-          </div>
-          <div className="text mt-5">
-            <p className="mb-5 font-bold text-xl">Hello</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-              commodi amet minus alias fugit magnam architecto?
-            </p>
-          </div>
-        </div>
-        <div className="bg-white m-5 text-center p-10 border-2">
-          <div className="icons flex justify-center">
-            <BsImages className="w-12 h-12 text-red-600" />
-          </div>
-          <div className="text mt-5">
-            <p className="mb-5 font-bold text-xl">Hello</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-              commodi amet minus alias fugit magnam architecto?
-            </p>
-          </div>
-        </div>
-        <div className="bg-white m-5 text-center p-10 border-2">
-          <div className="icons flex justify-center">
-            <BsWordpress className="w-12 h-12 text-red-600" />
-          </div>
-          <div className="text mt-5">
-            <p className="mb-5 font-bold text-xl">Hello</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-              commodi amet minus alias fugit magnam architecto?
-            </p>
-          </div>
-        </div>
-        <div className="bg-white m-5 text-center p-10 border-2">
-          <div className="icons flex justify-center">
-            <BsArrowsMove className="w-12 h-12 text-red-600" />
-          </div>
-          <div className="text mt-5">
-            <p className="mb-5 font-bold text-xl">Hello</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-              commodi amet minus alias fugit magnam architecto?
-            </p>
-          </div>
-        </div>
-        <div className="bg-white m-5 text-center p-10 border-2">
-          <div className="icons flex justify-center">
-            <BsGear className="w-12 h-12 text-red-600" />
-          </div>
-          <div className="text mt-5">
-            <p className="mb-5 font-bold text-xl">Hello</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-              commodi amet minus alias fugit magnam architecto?
-            </p>
-          </div>
-        </div>
-        <div className="bg-white m-5 text-center p-10 border-2">
-          <div className="icons flex justify-center">
-            <FaRocket className="w-12 h-12 text-red-600" />
-          </div>
-          <div className="text mt-5">
-            <p className="mb-5 font-bold text-xl">Hello</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-              commodi amet minus alias fugit magnam architecto?
-            </p>
-          </div>
-        </div>
+        {data.map((data) => {
+          return (
+            <div
+              className="bg-white m-5 text-center p-10 border-2"
+              key={data.id}
+            >
+              <div className="icons flex justify-center text-red-600">
+                {data.icon}
+              </div>
+              <div className="text mt-5">
+                <p className="mb-5 font-bold text-xl">{data.title}</p>
+                <p>{data.text}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
--- src/Service/service-data.js
+++ src/Service/service-data.js
@@ -5,37 +5,37 @@ import { FaRocket } from "react-icons/fa";
 const data = [
   {
     id: 1,
-    icon: AiFillHtml5,
+    icon: <AiFillHtml5 className="w-12 h-12" />,
     title: "Design Trends",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
   },
   {
     id: 2,
-    icon: BsImages,
+    icon: <BsImages className="w-12 h-12" />,
     title: "PSD Design",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
   },
   {
     id: 3,
-    icon: BsGear,
+    icon: <BsGear className="w-12 h-12" />,
     title: "Customer Support",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
   },
   {
     id: 4,
-    icon: BsWordpress,
+    icon: <BsWordpress className="w-12 h-12" />,
     title: "Web Development",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
   },
   {
     id: 5,
-    icon: BsArrowsMove,
+    icon: <BsArrowsMove className="w-12 h-12" />,
     title: "Fully Responsive",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
   },
   {
     id: 6,
-    icon: FaRocket,
+    icon: <FaRocket className="w-12 h-12" />,
     title: "Branding",
     text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
   },
