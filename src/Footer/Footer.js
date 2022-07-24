c98d339ff3e0815a36ba17a7a6f9fb207f7c517a
Alisahib Amiraslan <=>
2022-07-24 20:58:54 +0300

    dfgd

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -47,13 +47,13 @@ function Footer() {
       </div>
       <div className="copyright pl-0 lg:pl-64 pb-7 text-white text-center bg-black flex sm:justify-between justify-center sm:flex-row flex-col items-center px-5">
         <div className="icons flex gap-5 sm:justify-start justify-center">
-          <a href="https://github.com/AlisahibAmiraslan">
+          <a href="https://github.com/AlisahibAmiraslan" target="_blank">
             <BsGithub className="w-6 h-6" />
           </a>
-          <a href="/">
+          <a href="/" target="_blank">
             <BsInstagram className="w-6 h-6 text-red-800" />
           </a>
-          <a href="/">
+          <a href="/" target="_blank">
             <BsFacebook className="w-6 h-6 text-blue-600" />
           </a>
         </div>
