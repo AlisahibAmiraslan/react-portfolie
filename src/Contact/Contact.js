a4f3bf1c4ed60e169690b5f0f668455ac4c8e25e
Alisahib Amiraslan <=>
2022-07-24 15:52:57 +0300

    gkjgkj

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./contact.scss";
+import emailjs from "emailjs-com";
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_piylpri",
+        "template_eca068j",
+        form.current,
+        "tUACHYtswa-kASl0C"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
   return (
     <div className="w-full pl-0 sm:pl-64 contact">
       <div className="title flex pt-20 flex-col text-center">
@@ -11,43 +35,53 @@ function Contact() {
         <p className="text-gray-400 mt-3">Feel free to contact me anytime</p>
       </div>
       <div className="contact-me w-full py-16">
-        <div className="w-full px-10 grid gap-5 md:grid-cols-2 grid-cols-1">
-          <input
-            type="text"
-            id=""
-            placeholder="Name"
-            className="w-full p-2 focus:outline-none focus:border-red-600 border-b-2"
-          />
-          <input
-            type="mail"
-            id=""
-            placeholder="Email"
-            className="w-full p-2 focus:outline-none focus:border-red-600 border-b-2"
-          />
-        </div>
-        <div className="w-full px-10">
-          <input
-            type="text"
-            id=""
-            placeholder="Subject"
-            className="w-full p-2 my-5 focus:outline-none focus:border-red-600 border-b-2"
-          />
-        </div>
-        <div className="w-full px-10">
-          <textarea
-            id=""
-            placeholder="Message"
-            className="w-full p-2 pb-10 focus:outline-none focus:border-red-600 border-b-2"
-          ></textarea>
-        </div>
-        <div className="px-10 mt-5">
-          <button
-            type="submit"
-            className="px-5 py-3 bg-red-600 text-white rounded-3xl text-sm"
-          >
-            Send Message
-          </button>
-        </div>
+        <form onSubmit={sendEmail} ref={form}>
+          <div className="w-full px-10 grid gap-5 md:grid-cols-2 grid-cols-1">
+            <input
+              type="text"
+              id=""
+              placeholder="Name"
+              className="w-full p-2 focus:outline-none focus:border-red-600 border-b-2"
+              name="name"
+              required
+            />
+            <input
+              type="mail"
+              id="email"
+              placeholder="Email"
+              className="w-full p-2 focus:outline-none focus:border-red-600 border-b-2"
+              name="email"
+              required
+            />
+          </div>
+          <div className="w-full px-10">
+            <input
+              type="text"
+              id=""
+              placeholder="Subject"
+              className="w-full p-2 my-5 focus:outline-none focus:border-red-600 border-b-2"
+              name="subject"
+              required
+            />
+          </div>
+          <div className="w-full px-10">
+            <textarea
+              id=""
+              placeholder="Message"
+              className="w-full p-2 pb-10 focus:outline-none focus:border-red-600 border-b-2"
+              name="message"
+              required
+            ></textarea>
+          </div>
+          <div className="px-10 mt-5">
+            <button
+              type="submit"
+              className="px-5 py-3 bg-red-600 text-white rounded-3xl text-sm"
+            >
+              Send Message
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
