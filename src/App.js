48ecab5432535df415aff811a6945cb46b4fb3c6
Alisahib Amiraslan <=>
2022-07-25 22:31:28 +0300

    dsds

--- src/App.js
+++ src/App.js
@@ -5,17 +5,37 @@ import Work from "./Work/Work";
 import Portfolio from "./portfolio/Portfolio";
 import Contact from "./Contact/Contact";
 import Footer from "./Footer/Footer";
+import { useState } from "react";
+import { useEffect } from "react";
+import BounceLoader from "react-spinners/BounceLoader";
 
 function App() {
+  const [loading, setLoading] = useState();
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <>
-      <Home />
-      <About />
-      <Service />
-      <Work />
-      <Portfolio />
-      <Contact />
-      <Footer />
+      {loading ? (
+        <div className="loading bg-black w-full h-screen flex justify-center items-center">
+          <BounceLoader color={"#D0021B"} loading={loading} size={100} />
+        </div>
+      ) : (
+        <>
+          <Home />
+          <About />
+          <Service />
+          <Work />
+          <Portfolio />
+          <Contact />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
